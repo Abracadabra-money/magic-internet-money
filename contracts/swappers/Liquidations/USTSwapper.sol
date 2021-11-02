9fec2c03aac9ad6f85a2ad39a66c8cfc89067516
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2021-11-02 12:34:12 +0000

    Added new UST swappers

--- contracts/swappers/Liquidations/USTSwapper.sol
+++ contracts/swappers/Liquidations/USTSwapper.sol
@@ -4,7 +4,7 @@ import "../../interfaces/ISwapperGeneric.sol";
 
 interface CurvePool {
     function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy, address receiver) external returns (uint256);
-    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external returns (uint256);
+    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy, address receiver) external returns (uint256);
     function approve(address _spender, uint256 _value) external returns (bool);
     function add_liquidity(uint256[3] memory amounts, uint256 _min_mint_amount) external;
 }
@@ -16,18 +16,15 @@ interface IBentoBoxV1 {
 
 contract USTSwapper is ISwapperGeneric {
 
-     // Local variables
-    IBentoBoxV1 public constant bentoBox = IBentoBoxV1(0xF5BCE5077908a1b7370B9ae04AdC565EBd643966);
-    CurvePool public constant MIM3POOL = CurvePool(0x5a6A4D54456819380173272A5E8E9B9904BdF41B);
-    CurvePool constant public UST3POOL = CurvePool(0x890f4e345B1dAED0367A877a1612f86A1f86985f);
+    // Local variables
+    IBentoBoxV1 public constant degenBox = IBentoBoxV1(0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce);
+    CurvePool constant public UST2POOL = CurvePool(0x55A8a39bc9694714E2874c1ce77aa1E599461E18);
     IERC20 public constant MIM = IERC20(0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3);
     IERC20 public constant UST = IERC20(0xa47c8bf37f92aBed4A126BDA807A7b7498661acD);
-    IERC20 public constant ThreePOOL = IERC20(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490);
 
     constructor() public {
-        ThreePOOL.approve(address(MIM3POOL), type(uint256).max);
-        UST.approve(address(UST3POOL), type(uint256).max);
-        MIM.approve(address(bentoBox), type(uint256).max);
+        UST.approve(address(UST2POOL), type(uint256).max);
+        MIM.approve(address(degenBox), type(uint256).max);
     }
 
 
@@ -41,13 +38,11 @@ contract USTSwapper is ISwapperGeneric {
         uint256 shareFrom
     ) public override returns (uint256 extraShare, uint256 shareReturned) {
 
-        (uint256 amountFrom, ) = bentoBox.withdraw(UST, address(this), address(this), 0, shareFrom);
+        (uint256 amountFrom, ) = degenBox.withdraw(UST, address(this), address(this), 0, shareFrom);
 
-        uint256 amountIntermediate = UST3POOL.exchange(0, 1, amountFrom, 0);
+        uint256 amountTo = UST2POOL.exchange(1, 0, amountFrom, 0, address(degenBox));
 
-        uint256 amountTo = MIM3POOL.exchange(1, 0, amountIntermediate, 0);
-
-        (, shareReturned) = bentoBox.deposit(MIM, address(this), recipient, amountTo, 0);
+        (, shareReturned) = degenBox.deposit(MIM, address(degenBox), recipient, amountTo, 0);
         extraShare = shareReturned - shareToMin;
     }
 
