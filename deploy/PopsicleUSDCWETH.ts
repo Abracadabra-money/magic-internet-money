61b98210d1053d5e10aa08f2531d01260536675c
0xCalibur <0xCalibur@protonmail.com>
2021-11-03 12:10:35 -0400

    missing change

--- deploy/PopsicleUSDCWETH.ts
+++ deploy/PopsicleUSDCWETH.ts
@@ -8,7 +8,16 @@ const supportedChains = [ChainId.Mainnet, ChainId.Fantom, ChainId.BSC];
 
 const parameters = {
   [ChainId.Mainnet]: {
-    degenBox: "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce"
+    degenBox: "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce",
+    cauldronV2MasterContract: "0x476b1E35DDE474cB9Aa1f6B85c9Cc589BFa85c1F"
+  },
+  [ChainId.Fantom]: {
+    degenBox: "",
+    cauldronV2MasterContract: ""
+  },
+  [ChainId.BSC]: {
+    degenBox: "",
+    cauldronV2MasterContract: ""
   }
 };
 
@@ -18,9 +27,8 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
 
   const { deployer } = await getNamedAccounts();
   const chainId = await hre.getChainId();
-
   const parameters = supportedChains[chainId];
-  
+
   // Oracle
   await deploy("PopsicleUSDCWETHOracle", {
     from: deployer,
