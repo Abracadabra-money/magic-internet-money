a26ef662193b830a91cd4a6ffffd2c3dbaf9533e
0xCalibur <0xCalibur@protonmail.com>
2021-12-03 13:27:30 -0500

    set addresses for all other chains

--- deploy/MultichainWithdrawer.ts
+++ deploy/MultichainWithdrawer.ts
@@ -29,21 +29,28 @@ const ParametersPerChain = {
   [ChainId.BSC]: {
     bentoBox: "",
     degenBox: "0x090185f2135308BaD17527004364eBcC2D37e5F6",
-    mim: "",
-    anyswapRouter: "",
-    mimProvider: "",
+    mim: "0xfE19F0B51438fd612f6FD59C1dbB3eA319f433Ba",
+    anyswapRouter: "0xd1C5966f9F5Ee6881Ff6b261BBeDa45972B1B5f3",
+    mimProvider: "0x9d9bC38bF4A128530EA45A7d27D0Ccb9C2EbFaf6",
     bentoBoxCauldronsV2: [],
     bentoBoxCauldronsV1: [],
-    degenBoxCauldrons: [],
+    degenBoxCauldrons: [
+      "0x692CF15F80415D83E8c0e139cAbcDA67fcc12C90", // CAKE
+      "0xF8049467F3A9D50176f4816b20cDdd9bB8a93319" // wBNB
+    ],
     owner: xMerlin,
   },
   [ChainId.Fantom]: {
-    bentoBox: "",
+    bentoBox: "0xF5BCE5077908a1b7370B9ae04AdC565EBd643966",
     degenBox: "0x74A0BcA2eeEdf8883cb91E37e9ff49430f20a616",
-    mim: "",
-    anyswapRouter: "",
-    mimProvider: "",
-    bentoBoxCauldronsV2: [],
+    mim: "0x82f0B8B456c1A451378467398982d4834b6829c1",
+    anyswapRouter: "0x1CcCA1cE62c62F7Be95d4A67722a8fDbed6EEcb4",
+    mimProvider: "0xb4ad8B57Bd6963912c80FCbb6Baea99988543c1c",
+    bentoBoxCauldronsV2: [
+      "0x8E45Af6743422e488aFAcDad842cE75A09eaEd34", // wFTM 1
+      "0xd4357d43545F793101b592bACaB89943DC89d11b", // wFTM 2
+      "0xed745b045f9495B8bfC7b58eeA8E0d0597884e12", // yvWFTM
+    ],
     bentoBoxCauldronsV1: [],
     degenBoxCauldrons: [],
     owner: xMerlin,
@@ -51,10 +58,12 @@ const ParametersPerChain = {
   [ChainId.Arbitrum]: {
     bentoBox: "0x74c764D41B77DBbb4fe771daB1939B00b146894A",
     degenBox: "",
-    mim: "",
-    anyswapRouter: "",
-    mimProvider: "",
-    bentoBoxCauldronsV2: [],
+    mim: "0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A",
+    anyswapRouter: "0xC931f61B1534EB21D8c11B24f3f5Ab2471d4aB50",
+    mimProvider: "0xf46BB6dDA9709C49EfB918201D97F6474EAc5Aea",
+    bentoBoxCauldronsV2: [
+      "0xC89958B03A55B5de2221aCB25B58B89A000215E6" // wETH
+    ],
     bentoBoxCauldronsV1: [],
     degenBoxCauldrons: [],
     owner: xMerlin,
