d9c70f32b7219b4e0c2ca5c079f9acc6de36781b
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2022-03-28 16:30:18 -0400

    Added deployments

--- archive/deploy/MSpellSender.ts
+++ archive/deploy/MSpellSender.ts
@@ -7,6 +7,9 @@ import { xMerlin } from "../test/constants";
 
 const allowedChainArray = [ "1", "43114", "250", "42161"]
 const mspellAddress = {"1": "0x94635B2034cCEc3293b81D411Cd77C36c353f41d", "43114": "0xC1f1862dE85374378173566a8F3BE28DA3c3EC70", "250": "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce", "42161": "0x694808221d4F31d5849F2aBA08584E2C8f4b99ff"}
+const lz = {"1": "1", "43114": "6", "250": "12", "42161": "10"}
+const reporterAddress = {"250": "0xC1f1862dE85374378173566a8F3BE28DA3c3EC70", "43114": "0x15a2a96608b48ebfd80c31DA8a9bE340A354CD46","42161": "0x35F78eBb33B69d0006910913480F483271638053"}
+
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   
   const { deployments, getNamedAccounts, getChainId } = hre;
@@ -26,10 +29,14 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
   const mspellSender = await ethers.getContract<MSpellSender>("mSpellSender");
 
   for(let i in allowedChainArray) {
-    if((await mspellSender.isActiveChain(allowedChainArray[i])) !== true) {
+    if((await mspellSender.isActiveChain(lz[allowedChainArray[i]]))[0] != 1) {
       //const mspell = await ethers.getContract<MSpellStaking>(ChainName[allowedChainArray[i]] + "mSpellStaking");
       const address = mspellAddress[allowedChainArray[i]]
-      await mspellSender.addMSpellRecipient(address, allowedChainArray[i])
+      await mspellSender.addMSpellRecipient(address, allowedChainArray[i], lz[allowedChainArray[i]])
+    }
+
+    if(allowedChainArray[i] != "1" && reporterAddress[allowedChainArray[i]] && (await mspellSender.mSpellReporter(lz[allowedChainArray[i]])) !== reporterAddress[allowedChainArray[i]]) {
+      await mspellSender.addReporter(reporterAddress[allowedChainArray[i]], lz[allowedChainArray[i]])
     }
   }
 
@@ -41,8 +48,8 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
       address: mspellSender.address,
       constructorArguments: []
     });
-  } catch {
-    
+  } catch (error) {
+    console.error(error)
   }
   
 };
---
+++ archive/deploy/yvMIM3CRV.ts
@@ -0,0 +1,120 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, network } from "hardhat";
+import { ChainId } from "../utilities";
+import { DegenBox, ProxyOracle } from "../typechain";
+import { expect } from "chai";
+import { xMerlin } from "../test/constants";
+
+// List of supported chains to deploy on
+const supportedChains = [ChainId.Mainnet];
+
+export const ParametersPerChain = {
+  [ChainId.Mainnet]: {
+    cauldronDeploymentName: "yvMIM3CRVCauldron",
+    degenBox: "0xd96f48665a1410C0cd669A88898ecA36B9Fc2cce",
+    cauldronV2MasterContract: "0x476b1E35DDE474cB9Aa1f6B85c9Cc589BFa85c1F",
+    oracleData: "0x0000000000000000000000000000000000000000",
+    collateral: "0x2DfB14E32e2F8156ec15a2c21c3A6c053af52Be8", // yvCurve-MIM3CRV
+    proxyOracleDeploymentName: "YVMIM3CRVOracleProxy",
+    oracleName: "YVMIM3CRVOracle",
+    swapperName: "YVMIM3CRVSwapper",
+    levSwapperName: "YVMIM3CRVLevSwapper",
+  },
+};
+
+const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+  const chainId = await hre.getChainId();
+  const parameters = ParametersPerChain[parseInt(chainId)];
+
+  const INTEREST_CONVERSION = 1e18 / (365.25 * 3600 * 24) / 100;
+  const OPENING_CONVERSION = 1e5 / 100;
+
+  const collateralization = 75 * 1e3; // 75% LTV
+  const opening = 0.5 * OPENING_CONVERSION; // .5% borrow initial fee
+  const interest = parseInt(String(1 * INTEREST_CONVERSION)); // 1% Interest
+  const liquidation = 12.5 * 1e3 + 1e5; // 12.5% liquidation fee
+
+  // Proxy Oracle
+  await deploy(parameters.proxyOracleDeploymentName, {
+    from: deployer,
+    args: [],
+    log: true,
+    contract: "ProxyOracle",
+    deterministicDeployment: false,
+  });
+
+  // Oracle Implementation
+  await deploy(parameters.oracleName, {
+    from: deployer,
+    args: [],
+    log: true,
+    deterministicDeployment: false,
+  });
+
+  // Cauldron
+  const DegenBox = await ethers.getContractAt<DegenBox>("DegenBox", parameters.degenBox);
+  const ProxyOracle = await ethers.getContract<ProxyOracle>(parameters.proxyOracleDeploymentName);
+
+  let initData = ethers.utils.defaultAbiCoder.encode(
+    ["address", "address", "bytes", "uint64", "uint256", "uint256", "uint256"],
+    [parameters.collateral, ProxyOracle.address, parameters.oracleData, interest, liquidation, collateralization, opening]
+  );
+
+  const tx = await (await DegenBox.deploy(parameters.cauldronV2MasterContract, initData, true)).wait();
+
+  const deployEvent = tx?.events?.[0];
+  expect(deployEvent?.eventSignature).to.be.eq("LogDeploy(address,bytes,address)");
+
+  // Register the deployment so it's available within the test using `getContract`
+  deployments.save(parameters.cauldronDeploymentName, {
+    abi: [],
+    address: deployEvent?.args?.cloneAddress,
+  });
+
+  // Liquidation Swapper
+  await deploy(parameters.swapperName, {
+    from: deployer,
+    args: [],
+    log: true,
+    deterministicDeployment: false,
+  });
+
+  // Leverage Swapper
+  await deploy(parameters.levSwapperName, {
+    from: deployer,
+    args: [],
+    log: true,
+    deterministicDeployment: false,
+  });
+
+  const OracleImplementation = await ethers.getContract(parameters.oracleName);
+  if ((await ProxyOracle.oracleImplementation()) !== OracleImplementation.address) {
+    await ProxyOracle.changeOracleImplementation(OracleImplementation.address);
+  }
+  if ((await ProxyOracle.owner()) !== xMerlin) {
+    await ProxyOracle.transferOwnership(xMerlin, true, false);
+  }
+};
+
+export default deployFunction;
+
+if (network.name !== "hardhat") {
+  deployFunction.skip = ({ getChainId }) =>
+    new Promise(async (resolve, reject) => {
+      try {
+        getChainId().then((chainId) => {
+          resolve(supportedChains.indexOf(parseInt(chainId)) === -1);
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+}
+
+deployFunction.tags = ["yvMIM3CRV"];
+deployFunction.dependencies = [];
