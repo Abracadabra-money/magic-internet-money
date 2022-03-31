219e1eac90b188b43b7ab84402f306c31fdfc8f9
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2022-03-31 02:16:33 -0400

    FINAL FINAL FINAL

--- archive/deploy/MSpellReporter.ts
+++ archive/deploy/MSpellReporter.ts
@@ -12,7 +12,7 @@ const endpoints = {
   "42161": "0x3c2269811836af69497E5F486A85D7316753cf62"
 }
 
-const mSpellSender = "0x3D94B714382F61107a71690463B921E1Db6b6735"
+const mSpellSender = "0x54c80f1c4718E3E6df71b83BB9Cb389EC41F07a5"
 const allowedChainArray = [ "43114", "250", "42161"]
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre;
