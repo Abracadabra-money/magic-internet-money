1068196d14138d9a572593273c31d4d0d97fb13f
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2022-04-19 21:13:47 -0400

    Corrected Comments

--- contracts/interfaces/IWhitelister.sol
+++ contracts/interfaces/IWhitelister.sol
@@ -8,10 +8,10 @@ interface IWhitelister {
     /// @return success if user is allowed to borrow said new amount, returns true otherwise false
     function getBorrowStatus(address user, uint256 newBorrowPart) external view returns (bool success);
 
-    /// @notice View function to check
+    /// @notice Function for the user to bring a merkle proof to set a new max borrow
     /// @param user address of the user
     /// @param maxBorrow new max borrowPart for the user. 
-    /// @param merkleProof merkle proof provided to user to show ownership
+    /// @param merkleProof merkle proof provided to user.
     /// @return success if the user is indeed allowed to borrow said new amount
     function setMaxBorrow(address user, uint256 maxBorrow, bytes32[] calldata merkleProof) external returns (bool success);
 }
