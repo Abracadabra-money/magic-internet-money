c2e52981daadf4e1094ceb3cda1ac466c7636869
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2022-05-21 18:52:28 +0200

    Fixed Merkle Root Generator

--- scripts/generate_merkle_root.ts
+++ scripts/generate_merkle_root.ts
@@ -36,27 +36,34 @@ const hashWhitelistNode = (account: string, maxBorrow: string) => {
   };
 
 const createMerkle = (json: {string: {"userBorrowPart": string}}) => {
-    type Output = {"merkleRoot": string, string: {"userBorrowPart": string, "leaf": string}}
+    type Output = {"merkleRoot": string, "total": string, string: {"userBorrowPart": string, "leaf": string}}
     let outputObj:  Output | {} = {};
     let leaves:Buffer[] = []
     type BorrowPart = {userBorrowPart: string};
+    let intermediate = {}
+    let total = ethers.BigNumber.from(0)
     for (const [key, value] of Object.entries(json)) {
         if(value.userBorrowPart != "0") {
             let borrowPart = Number(value.userBorrowPart).toLocaleString('fullwide', { useGrouping: false })
+            total = total.add(ethers.BigNumber.from(borrowPart).div(ethers.BigNumber.from("1000000000000000000")))
             let hash = hashWhitelistNode(key, borrowPart)
             leaves.push(hash)
             outputObj[key] = {}
             outputObj[key].userBorrowPart = borrowPart;
             outputObj[key].leaf = ethers.utils.hexlify(hash);
+            intermediate[key] = hash;
         }
     }
-    leaves = leaves.sort();
     
-    const merkleTree = new MerkleTree(leaves, item => ethers.utils.keccak256(item));
+    const merkleTree = new MerkleTree(leaves, item => ethers.utils.keccak256(item), { sortPairs: true });
+    
     let merkleRoot = merkleTree.getHexRoot();
     for (const [key, value] of Object.entries(outputObj)) {
-        outputObj[key].proof = merkleTree.getHexProof(outputObj[key].leaf)
+        outputObj[key].proof = merkleTree.getHexProof(intermediate[key])
     }
+
+    outputObj["total"] = total.toString()
+
     outputObj["merkleRoot"] = merkleRoot;
     return outputObj;
 }
